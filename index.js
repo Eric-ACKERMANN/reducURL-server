921857438b3662c0d72369c8c10a20fe23da29c1
ACKERMANN Eric <ackermann@macbook-pro-de-ackermann.home>
2019-06-14 23:10:39 +0200

    add CORS

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 
 mongoose.connect(
